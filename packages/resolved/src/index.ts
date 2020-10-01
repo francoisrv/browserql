7eec45405e37d6d87b2a3f9ecc975ae7ba748049
Francois <fra@pop-os.localdomain>
2020-10-02 01:18:06 +0200

    contracts v1.1.0

--- packages/resolved/src/index.ts
+++ packages/resolved/src/index.ts
@@ -1,1 +1,39 @@
-export default function resolve() {}
+import { DocumentNode, print } from 'graphql'
+import makeContracts from '@browserql/contracts'
+
+type Operation<T> = {
+  [name in keyof T]: (
+    variables: any
+  ) =>
+    | {
+        query: DocumentNode
+        variables: any
+      }
+    | {
+        mutation: DocumentNode
+        variables: any
+      }
+}
+
+export default function resolve(document: string | DocumentNode) {
+  const contracts = makeContracts(document)
+  const Query: Partial<Operation<typeof contracts>> = {}
+  const Mutation: Partial<Operation<typeof contracts>> = {}
+  for (const query in contracts.Query) {
+    console.log(print(contracts.Query[query]))
+    Query[query as keyof typeof contracts] = (variables = {}) => ({
+      query: contracts.Query[query],
+      variables,
+    })
+  }
+  for (const mutation in contracts.Mutation) {
+    Mutation[mutation as keyof typeof contracts] = (variables = {}) => ({
+      mutation: contracts.Mutation[mutation],
+      variables,
+    })
+  }
+  return {
+    Query,
+    Mutation,
+  }
+}
