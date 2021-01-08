8bab2acf23bf2ea26e1edb954a2996e17aa8cdf5
Pierre Drouillet <pierrot@pop-os.localdomain>
2021-01-08 14:21:42 +0100

    Add more documentation

--- packages/react/src/hoc/withMutation.tsx
+++ packages/react/src/hoc/withMutation.tsx
@@ -1,50 +1,50 @@
-import type { DocumentNode } from 'graphql'
-import React, { ComponentType } from 'react'
-import { Z_UNKNOWN } from 'zlib'
-import { UseMutation } from '..'
+// import type { DocumentNode } from 'graphql'
+// import React, { ComponentType } from 'react'
+// import { Z_UNKNOWN } from 'zlib'
+// import { UseMutation } from '..'
 
-interface HOCMutation<DATA, VARIABLES> {
-  execute: (v: VARIABLES) => DATA
-  loading: boolean
-  error: Error | undefined
-  data: DATA
-}
+// interface HOCMutation<DATA, VARIABLES> {
+//   execute: (v: VARIABLES) => DATA
+//   loading: boolean
+//   error: Error | undefined
+//   data: DATA
+// }
 
-export type WithMutationProps<
-  PROP_NAME extends string,
-  DATA = unknown,
-  VARIABLES = unknown
-> = { [P in PROP_NAME]: HOCMutation<DATA, VARIABLES> }
+// export type WithMutationProps<
+//   PROP_NAME extends string,
+//   DATA = unknown,
+//   VARIABLES = unknown
+// > = { [P in PROP_NAME]: HOCMutation<DATA, VARIABLES> }
 
-export default function withMutation<NAME extends string>(name: NAME) {
-  return function <PROPS = unknown, DATA = unknown, VARIABLES = unknown>(
-    mutation: DocumentNode
-  ) {
-    return function (
-      Component: ComponentType<PROPS & WithMutationProps<NAME, DATA, VARIABLES>>
-    ) {
-      return function (props: PROPS) {
-        return (
-          <UseMutation mutation={mutation}>
-            {(mutate, { loading, error, data }) => {
-              const mergedProps: WithMutationProps<NAME, DATA, VARIABLES> = {
-                [name]: {
-                  execute: mutate,
-                  loading,
-                  error,
-                  data,
-                },
-              }
-              const nextProps: PROPS &
-                WithMutationProps<NAME, DATA, VARIABLES> = {
-                ...props,
-                ...mergedProps,
-              }
-              return <Component {...nextProps} />
-            }}
-          </UseMutation>
-        )
-      }
-    }
-  }
-}
+// export default function withMutation<NAME extends string>(name: NAME) {
+//   return function <PROPS = unknown, DATA = unknown, VARIABLES = unknown>(
+//     mutation: DocumentNode
+//   ) {
+//     return function (
+//       Component: ComponentType<PROPS & WithMutationProps<NAME, DATA, VARIABLES>>
+//     ) {
+//       return function (props: PROPS) {
+//         return (
+//           <UseMutation mutation={mutation}>
+//             {(mutate, { loading, error, data }) => {
+//               const mergedProps: WithMutationProps<NAME, DATA, VARIABLES> = {
+//                 [name]: {
+//                   execute: mutate,
+//                   loading,
+//                   error,
+//                   data,
+//                 },
+//               }
+//               const nextProps: PROPS &
+//                 WithMutationProps<NAME, DATA, VARIABLES> = {
+//                 ...props,
+//                 ...mergedProps,
+//               }
+//               return <Component {...nextProps} />
+//             }}
+//           </UseMutation>
+//         )
+//       }
+//     }
+//   }
+// }
--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,10 +1,15 @@
 export { default as BrowserqlContext } from './contexts/BrowserqlContext'
-export { default as BrowserqlMutation } from './components/BrowserqlMutation'
+
 export { default as BrowserqlProvider } from './components/BrowserqlProvider'
+
 export * from './components/BrowserqlProvider'
-export { default as BrowserqlQuery } from './components/BrowserqlQuery'
+
 export { default as UseMutation } from './components/UseMutation'
+
 export { default as UseQuery } from './components/UseQuery'
-export { default as withQuery } from './components/withQuery'
-export { default as withMutation } from './hoc/withMutation'
-export * from './hoc/withMutation'
+
+// export { default as withQuery } from './hoc/withQuery'
+
+// export { default as withMutation } from './hoc/withMutation'
+
+// export * from './hoc/withMutation'
