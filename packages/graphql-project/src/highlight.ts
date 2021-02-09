065dd4204d58040540203ae812191b70143ce3a6
Pierre Drouillet <drouilletpierre@gmail.com>
2021-02-09 03:49:29 +0100

    Add help for graphql cli

--- packages/graphql-project/src/highlight.ts
+++ packages/graphql-project/src/highlight.ts
@@ -1,6 +1,21 @@
 import { parse } from 'graphql'
-import { getFields, getKind, getName, getTypes } from '@browserql/fpql'
+import {
+  getDirectives,
+  getFields,
+  getKind,
+  getName,
+  getTypes,
+  parseKind,
+} from '@browserql/fpql'
 import colors from 'colors'
+
+function highlightCode(kind: string) {
+  const parsed = parseKind(kind)
+  if (parsed.required) {
+    return colors.bold
+  }
+  return colors.hidden
+}
 
 export default function highlight(code: string) {
   const schema = parse(code)
@@ -9,17 +24,33 @@ export default function highlight(code: string) {
   defs.forEach((def) => {
     if (def.kind === 'ObjectTypeDefinition') {
       const fields = getFields(def)
+      const directives = getDirectives(def)
+      const parsed = directives.map((directive) => `@${getName(directive)}`)
       console.log(
         colors.magenta('type'),
         colors.yellow(getName(def)),
-        fields.length ? colors.bold('{') : ''
+        parsed.join(' ').length < 80
+          ? parsed.join(' ').concat(fields.length ? colors.bold(' {') : '')
+          : '',
+        fields.length && !directives.length ? colors.bold('{') : ''
       )
+      if (directives.length && parsed.join(' ').length >= 80) {
+        directives.forEach((directive) => {
+          console.log(' ', `@${getName(directive)}`)
+        })
+        if (fields.length) {
+          console.log(colors.bold('{'))
+        }
+      }
       if (fields.length) {
         fields.forEach((field) => {
+          const parsed = parseKind(getKind(field))
           console.log(
             '  ',
-            getName(field).concat(colors.grey(':')),
-            getKind(field)
+            parsed.required
+              ? colors.bold(getName(field).concat(colors.bold(':')))
+              : getName(field).concat(colors.bold(':')),
+            colors.yellow(getKind(field))
           )
         })
         console.log(colors.bold('}'))
